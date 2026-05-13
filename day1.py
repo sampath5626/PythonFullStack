e5c9878efdda102f24b44859baac2d399b3e622a
sampath5626 <sampathramakrishnapedapati@gmail.com>
2026-05-13 15:34:11 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,53 @@
+print("This is my first code")
+
+'''
+WHAT IS PYTHON??
+-high level language
+------------------------
+-->no need to worry about low level management(stroage)
+
+-dynamically typed language
+----------------------------
+-->No need to mention the kind of data taking to varible
+
+-Interperter language
+---------------------
+--> line-by-line execution can be done in the python and can throw the error if any but all before line will execute and gives output
+
+-TOkENS
+-------
+--> keywords in the python
+if, else, while, for,......
+
+-IDENTIFIERS
+------------
+-->names of variables, functions and classes
+
+-VARIABLES
+----------
+-->GOOD WAYS : A-Z, a-z, _
+-->BAD WAYS : special characters, space, 0-9
+
+'''
+
+# Create a variable how it should be and how it should not be
+#how it should be 
+person = "maria"
+ball070 = 25
+name_age = {"sam": 21}
+
+#How it shouldn't be
+78name = ball
+
+
+
+
+
+
+
+
+
+
+
+
+
