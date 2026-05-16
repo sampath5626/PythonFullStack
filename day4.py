44a380f2b6e439a66f21ff780514efbf2664ac59
sampath5626 <sampathramakrishnapedapati@gmail.com>
2026-05-16 12:28:22 +0530

    Add files via upload
    learning pythonfullstack #day 4

---
+++ day4.py
@@ -0,0 +1,72 @@
+'''
+CONCADINATION
+---------------
+--> The (+) for the int  and can add but for the
+other data type it will act as concatinating the data type
+
+
+-TUPLE
+-------
+--> Collection of different data types sperated by comas,
+represented in "()" and immutable
+
+example : num1 = (1,"pyhthon",[1,2,3])
+          print(num1([2][1]))
+
+method
+-------
+count()
+-------
+--> this is used to count the particular item in the tuple
+syntax --> variable_name.count(item)
+
+index()
+--------
+used to find the index position of the iten and only gives the
+first occurance
+b = (1,2,["sam",23,1.2],"happy")
+print(b[2][0][2])
+print(b.index("happy"))
+
+DICTIONARY
+-----------
+--> Dict is a key:value pair, key and value pair seperated
+by ":" and pair is separated by ","
+--> represented by "{}"
+
+METHODS
+--------
+keys()
+------
+-->used to get all the keys forn the dict
+-syntax: dict.keys()
+
+values()
+--------
+-->used to get all the values from he dict
+-syntax: dict.values()
+
+update()
+---------
+ 
+
+
+
+
+
+'''
+
+ban = {"name":"hem",
+       (1,21):[1,2,3],
+       "salary":15000,
+       "name":"sampath",
+       1:56}
+print(ban.update({1:236}))
+print(ban[1])
+print(ban)
+ban["adress"] = "thanam"
+print(ban)
+
+
+
+
