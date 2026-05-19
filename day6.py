ccde2778d8b81efd6382c1a90b6283ae097e0617
sampath5626 <sampathramakrishnapedapati@gmail.com>
2026-05-19 15:04:30 +0530

    Add files via upload
    day 6 of learning python

---
+++ day6.py
@@ -0,0 +1,77 @@
+'''
+Type Converstions
+------------------
+int-->
+an = 78
+us = str(an)
+om = float(an)
+print(om)
+print(type(om))
+print(type(us)
+
+str-->
+an = "python"
+b = list(an)
+print(b)
+
+float-->
+car = 90.8
+print(int(car))
+print(type(str(cat)))
+
+List-->
+Any = [6,7]
+print(str()Any)
+print(tuple(Any))
+
+int as a user-input
+--------------------
+num = int(input("Enter a number :"))
+print(67+num)
+
+string as a user-input
+----------------------
+some = input("my name is charan")
+
+list as a user-input
+---------------------
+any = list(map(int,input("enter numbers:").split()))
+print(any)
+
+tuple as a user-input
+---------------------
+any = tuple(map(int,input("enter numbers:").split()))
+print(any)
+
+any = eval(input("enter numbers:"))
+print(type(any))
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
