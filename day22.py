92a7a579c7b405157250af1442f70be685bed987
sampath5626 <sampathramakrishnapedapati@gmail.com>
2026-06-11 15:34:01 +0530

    Add files via upload

---
+++ day22.py
@@ -0,0 +1,30 @@
+'''
+Error Handling
+==============
+try block
+---------
+--> the try block,test a block of code for error
+
+Except block
+-------------
+-->The except block let handle if the code cntain errors...
+
+else block
+-----------
+This  will be exicuted, if the try block has no error in the code
+
+finally  block
+--------------
+This will executed either try block contain error or not...
+'''
+
+try:
+    print(a)
+except:
+    print("it an error")
+else:
+    print("There is no error")
+finally:
+    print("The End")
+   
+
